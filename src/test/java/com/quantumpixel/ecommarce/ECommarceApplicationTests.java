db09229afd84dd218fd84b81bc462a22c28d1206
Indrit-Vaka <63372867+Indrit-Vaka@users.noreply.github.com>
2023-11-29 15:36:05 +0100

    Initializing repository

---
+++ src/test/java/com/quantumpixel/ecommarce/ECommarceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantumpixel.ecommarce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommarceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
