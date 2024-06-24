23a596d6e0d715c9027d87534985af79155b542c
Indrit Vaka <indrit.vaka-extern@gmail.com>
2024-06-24 17:01:59 +0200

    [IV] QPT-72 fixed integration tests

--- src/integTest/java/com/quantum_pixel/arg/config/ApplicationConfigIT.java
+++ src/integTest/java/com/quantum_pixel/arg/config/ApplicationConfigIT.java
@@ -1,17 +1,19 @@
 package com.quantum_pixel.arg.config;
 
 import com.quantum_pixel.arg.AppITConfig;
+import com.quantum_pixel.arg.ConfigTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @AppITConfig
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class ApplicationConfigIT {
+class ApplicationConfigIT extends ConfigTest {
 
     @Autowired
     ApplicationConfig applicationConfig;
@@ -24,5 +26,10 @@ class ApplicationConfigIT {
         // then
         assertThat(restTemplate)
                 .isNotNull();
+    }
+
+    @Override
+    protected List<String> getTableToTruncate() {
+        return List.of();
     }
 }
