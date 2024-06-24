fb9b52e2545003f6850435f8f2a05a4c54fcb269
Indrit Vaka <indrit.vaka-extern@gmail.com>
2024-06-24 15:54:01 +0200

    [IV] QPT-72 write unit tests

--- src/integTest/java/com/quantum_pixel/arg/config/ApplicationConfigIT.java
+++ src/integTest/java/com/quantum_pixel/arg/config/ApplicationConfigIT.java
@@ -3,12 +3,14 @@ package com.quantum_pixel.arg.config;
 import com.quantum_pixel.arg.AppITConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @AppITConfig
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ApplicationConfigIT {
 
     @Autowired
