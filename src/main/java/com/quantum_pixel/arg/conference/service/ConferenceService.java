fe267a0e281ca4bd1e0ff90dcb4d851886a582b2
Indrit Vaka <indrit.vaka-extern@gmail.com>
2024-07-30 08:43:04 +0200

    [IV] QPT-176 completed hotel reservation

--- src/main/java/com/quantum_pixel/arg/conference/service/ConferenceService.java
+++ src/main/java/com/quantum_pixel/arg/conference/service/ConferenceService.java
@@ -2,11 +2,10 @@ package com.quantum_pixel.arg.conference.service;
 
 import com.quantum_pixel.arg.conference.model.ConferenceMailStructure;
 import com.quantum_pixel.arg.conference.web.mapper.ConferenceMapper;
+import com.quantum_pixel.arg.hotel.exception.PastDateException;
 import com.quantum_pixel.arg.v1.web.model.ConferenceMailStructureDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
@@ -32,7 +31,7 @@ public class ConferenceService {
                                 || reservation.getStartTime().isAfter(reservation.getEndTime())
                 ).findFirst()
                 .ifPresent(sam -> {
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Reservation date can not be in the past");
+                    throw new PastDateException("Reservation date can not be in the past");
                 });
 
     }
