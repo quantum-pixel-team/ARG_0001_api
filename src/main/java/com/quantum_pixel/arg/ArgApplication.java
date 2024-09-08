86129b5bddba3e96440b86b9ba5b82330c6003b6
Indrit Vaka <indrit.vaka@gmail.com>
2024-09-08 13:20:19 +0200

    [IV] QPT-178 added extra fields to logging

--- src/main/java/com/quantum_pixel/arg/ArgApplication.java
+++ src/main/java/com/quantum_pixel/arg/ArgApplication.java
@@ -2,12 +2,16 @@ package com.quantum_pixel.arg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.MDC;
 
 @SpringBootApplication
 
 public class ArgApplication {
 
     public static void main(String[] args) {
+        MDC.put("component", "API");
+        MDC.put("app_id", "ARG_0001");
+
         SpringApplication.run(ArgApplication.class, args);
     }
 
